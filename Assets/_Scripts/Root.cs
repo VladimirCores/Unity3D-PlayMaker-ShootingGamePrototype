57f4df358fe34896d2bd07ff801da40a275bcaee
Vladimir Minkin <Vladimir.Minkin@massive.co>
2018-04-09 14:26:06 +0200

    Initial Commit

---
+++ Assets/_Scripts/Root.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+public class Root : MonoBehaviour {
+
+	[Range(1, 20)]
+	public int botsAmout; 
+
+	[Range(10, 40)]
+	public int bulletsAmout;
+
+	private PlayMakerFSM rootFSM;
+
+	// Use this for initialization
+	void Start () {
+		FsmVariables.GlobalVariables.FindFsmInt ("CONST_BOTS_AMOUNT").Value = botsAmout;
+		FsmVariables.GlobalVariables.FindFsmInt ("CONST_BULLETS_AMOUNT").Value = bulletsAmout;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
